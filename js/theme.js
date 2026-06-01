01bdf06ce602434ab080ebea6a3e581f4e41a360
Sojin Oh <sojinohh@gmail.com>
2026-05-31 18:09:53 -0700

    Theme change to green

---
+++ js/theme.js
@@ -0,0 +1,3 @@
+const THEMES = ['sky','purple','crimson','forest','midnight','rosegold','emerald',
+                 'sunset','mint','lavender','coral','teal','gold','slate','blush',
+                 'sage','indigo','peach','arctic','mauve'];
